0625e2dfdaeb6b0bbc683f01b9eae5befa4db919
Freddy Snijder <freddy@visionscapers.com>
2021-10-20 23:20:26 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="botshop",
-    version="0.0.21",
+    version="0.0.22",
     author="Freddy Snijder",
     author_email="botshop@visionscapers.com",
     description="Framework for chatbot model inference, optimized for neural conversation models.",
